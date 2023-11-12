9f72eceb4b987172275556b28f34b0d94ae2d02b
Ivan <135256901+IvLivanov@users.noreply.github.com>
2023-11-12 09:14:12 +0000

    database connection

--- Hello.py
+++ Hello.py
@@ -1,57 +1,32 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-import base64
 from streamlit.logger import get_logger
+from st_files_connection import FilesConnection
+import s3fs
 
 LOGGER = get_logger(__name__)
 
-def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
-        md = f"""
-            <audio controls autoplay="true">
-            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-            </audio>
-            """
-        st.markdown(
-            md,
-            unsafe_allow_html=True,
-        )
-
-
 
 def run():
-    
     st.set_page_config(
-        page_title="First change",
-        
-
+        page_title="Hello",
+        page_icon="👋"
     )
-    # Greetings text:
-    f = open("greetings_text.txt", "r")
-    for line in f:
-         st.write(line)
-    
-    #Greetings audio:
 
- 
-    # for playing note.wav file
-    autoplay_audio("My recording 21.mp3")
- 
+    # Create S3 file system object
+    s3 = s3fs.S3FileSystem()
+
+    # Read current content from S3
+    with s3.open("streamlitgreetingscard/greetings.txt", "r") as file:
+        current_content = file.read()
+        st.write(current_content)
+
+    # Play audio if file is uploaded
+    with s3.open("streamlitgreetingscard/audio_file.mp3", "rb") as audio_file:
+        audio_content = audio_file.read()
+        st.audio(audio_content, format='audio/mp3', start_time=0)
+
+
+    
 
 if __name__ == "__main__":
     run()
